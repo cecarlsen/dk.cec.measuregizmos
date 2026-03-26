b9badb2f7cdd13311297c103a82967cca4bf53e3
Carl Emil Carlsen <34234131+cecarlsen@users.noreply.github.com>
2026-03-26 19:20:31 +0100

    Update TransformGizmo.cs

--- Runtime/TransformGizmo.cs
+++ Runtime/TransformGizmo.cs
@@ -1,8 +1,9 @@
 ﻿/*
-	Copyright © Carl Emil Carlsen 2025
+	Copyright © Carl Emil Carlsen 2025-2026
 	http://cec.dk
 */
 
+using System;
 using UnityEngine;
 
 namespace MeasureGizmos
@@ -11,13 +12,25 @@ namespace MeasureGizmos
 	public class TransformGizmo : BaseGizmo
 	{
 		[SerializeField] Color _color = Color.yellow;
+		[SerializeField] bool _drawAxis = false;
 
 
 		protected override void Draw()
 		{
-			Gizmos.color = _color;
 			Gizmos.matrix = transform.localToWorldMatrix;
-			Gizmos.DrawWireCube( Vector3.zero, Vector3.one );
+
+			if( _color.a > 0f ){
+				Gizmos.color = _color;
+				Gizmos.DrawWireCube( Vector3.zero, Vector3.one );
+			}
+			if( _drawAxis ){
+				Gizmos.color = Color.red;
+				Gizmos.DrawLine( Vector3.zero, Vector3.right );
+				Gizmos.color = Color.green;
+				Gizmos.DrawLine( Vector3.zero, Vector3.up );
+				Gizmos.color = Color.blue;
+				Gizmos.DrawLine( Vector3.zero, Vector3.forward );
+			}
 		}
 	}
 }
